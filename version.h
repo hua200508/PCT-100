92c1a39786368331bbf6c3ec50346ed9b056a98e
hua <3374578300@qq.com>
2026-05-27 14:52:34 +0800

    初始提交：ESP32 项目

---
+++ version.h
@@ -0,0 +1,15 @@
+#ifndef __VERSION_H
+#define __VERSION_H
+
+
+#define VERSION    "V1.0"
+
+#define DEVICE_ID "PCT_100_001"  // 设备ID
+
+/* 项目信息 */
+#define PROJECT_NAME "PCT_100_CTL"
+#define PROJECT_AUTHOR "md"
+#define PROJECT_DATE "2026-05-27"
+   
+
+#endif
